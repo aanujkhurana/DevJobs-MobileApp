9dffad1b86ddc8ff3210908284f1a79cc0d50a53
aanujkhurana <aanujkhurana@gmail.com>
2024-03-06 18:02:30 +1000

    Added pouplar jobs

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,9 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-// import Config from "react-native-config";
-
-
 const useFetch = (endpoint, query) => {
     const [data, setData ] = useState(null);
     const [error, setError] = useState(null);
@@ -12,7 +9,7 @@ const useFetch = (endpoint, query) => {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            'X-RapidAPI-Key': '',
+            'X-RapidAPI-Key':  "",
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
         params: { ...query },
@@ -23,7 +20,7 @@ const useFetch = (endpoint, query) => {
         try {
             const response = await axios.request(options);
             setData(response.data.data);
-            console.log(response.data.data);
+            // console.log(response.data.data);
             setIsLoading(false);
         } catch (error) {
             setError(error);

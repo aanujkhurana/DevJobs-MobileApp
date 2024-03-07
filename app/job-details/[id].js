b48e8e870d7b92fac1dc05d2e2479f26fec76926
aanujkhurana <aanujkhurana@gmail.com>
2024-03-07 16:47:23 +1000

    jobDetails⏳

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,32 +1,20 @@
-import { Stack, useRouter, useLocalSearchParams } from "expo-router";
+import { Stack, useRouter, useGlobalSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
-import {
-  View,
-  Text,
-  SafeAreaView,
-  ScrollView,
-  ActivityIndicator,
-  RefreshControl,
-} from "react-native";
-
-import {
-  Company,
-  JobAbout,
-  JobFooter,
-  JobTabs,
-  ScreenHeaderBtn,
-  Specifics,
-} from "../../components";
+import {View,Text,SafeAreaView,ScrollView,ActivityIndicator,RefreshControl} from "react-native";
+import {Company,JobAbout,JobFooter,JobTabs,ScreenHeaderBtn,Specifics} from "../../components";
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
 const jobDetails = () => {
-    const params = useLocalSearchParams();
+    const params = useGlobalSearchParams();
     const router = useRouter();
 
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {})
     const { data, isLoading, error, refetch } = useFetch("job-details", {
         job_id: params.id,
-      });
+    });
 
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -39,17 +27,47 @@ const jobDetails = () => {
                         <ScreenHeaderBtn
                             iconUrl={icons.left}
                             dimension="60%"
-                            onPress={() => router.goBack()}/>
+                            handlePress={() => router.back()}/>
                     ),
                     headerRight: () => (
                         <ScreenHeaderBtn
                             iconUrl={icons.share}
-                            dimension="60%"
-                            onPress={() => router.goBack()}/>
+                            dimension="60%"/>
                     ),
-                    headerTitle: ' '
+                    headerTitle: ''
                 }}
-            ></Stack.Screen>
+            />
+            <>
+            <ScrollView
+            showsVerticalScrollIndicator={false}
+            refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
+            >
+                {isLoading ? (
+                    <ActivityIndicator size="large" color={COLORS.primary}/>
+                ) : error ?(
+                    <Text></Text>
+                ): data.length === 0 ? (
+                    <Text>No data available</Text>
+                  ) : (
+                    <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+                      <Company
+                        companyLogo={data[0].employer_logo}
+                        jobTitle={data[0].job_title}
+                        companyName={data[0].employer_name}
+                        location={data[0].job_country}
+                      />
+        
+                      <JobTabs
+                        tabs={tabs}
+                        activeTab={activeTab}
+                        setActiveTab={setActiveTab}
+                      />
+        
+                      {displayTabContent()}
+                    </View>
+                )}
+            </ScrollView>
+            </>
         </SafeAreaView>
     )
 }

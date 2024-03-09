600e85b7353b328ebb9cb6b6fded6d4b8edaf5dc
aanujkhurana <aanujkhurana@gmail.com>
2024-03-09 16:39:47 +1000

    fixing...

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -47,23 +47,17 @@ const jobDetails = () => {
                 ) : error ?(
                     <Text></Text>
                 ): data.length === 0 ? (
-                    <Text>No data available</Text>
-                  ) : (
-                    <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-                      <Company
-                        companyLogo={data[0].employer_logo}
-                        jobTitle={data[0].job_title}
-                        companyName={data[0].employer_name}
-                        location={data[0].job_country}
-                      />
-        
-                      <JobTabs
-                        tabs={tabs}
-                        activeTab={activeTab}
-                        setActiveTab={setActiveTab}
-                      />
-        
-                      {displayTabContent()}
+                    <Text>No data found</Text>
+                ) : (
+                    console.log(data),
+                    <View style={{padding: SIZES.medium, paddingBottom: 100}}>
+                    <Company
+                        companyLogo={data[0]?.employer_logo}
+                        jobTitle={data[0]?.job_title}
+                        companyName={data[0]?.employer_name}
+                        location={data[0]?.job_country}
+                    />
+                    <JobTabs job={data}/>
                     </View>
                 )}
             </ScrollView>

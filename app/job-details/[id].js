3effb49f00347e72ef7251cd5045a2b6c97b204a
aanujkhurana <aanujkhurana@gmail.com>
2024-03-10 19:43:31 +1000

    jobDetails⏳

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -5,7 +5,7 @@ import {Company,JobAbout,JobFooter,JobTabs,ScreenHeaderBtn,Specifics} from "../.
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
-const tabs = ["About", "Qualification", "Responsibilities"];
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const jobDetails = () => {
     const params = useLocalSearchParams();
@@ -16,25 +16,41 @@ const jobDetails = () => {
 
     const [activeTab, setActiveTab] = useState(tabs[0])
 
-    const onRefresh = useCallback(() => {})
-
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        refetch()
+        setRefreshing(false)
+    }, [])
     const { data, isLoading, refetch } = useFetch("job-details", {
         job_id: params.id,
     });
-
-    const displayTabContent = (tab, data) => {
+    const displayTabContent = () => {
         switch (activeTab) {
-            case "Qualification":
-                return <Specifics 
-                    title="Qualifications"
-                    points={data[0].job_highlights?.Qualifications ?? ("N/a")} 
-                    />;
-            default:
-                break;
+          case "Qualifications":
+            return (
+              <Specifics
+                title='Qualifications'
+                points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+              />
+            );
+    
+          case "About":
+            return (
+              <JobAbout info={data[0].job_description ?? "No data provided"} />
+            );
+    
+          case "Responsibilities":
+            return (
+              <Specifics
+                title='Responsibilities'
+                points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+              />
+            );
+    
+          default:
+            return null;
         }
-    }
-
-    console.log("data", data);
+      };
 
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite }}>
